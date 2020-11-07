468bd4ed06af38ae6a00dfc95e478e06f2c5c174
vinayak-2007 <73356466+vinayak-2007@users.noreply.github.com>
2020-11-07 18:44:58 +0530

    BULLETS AND WALLS

---
+++ bullets-walls-main/sketch.js
@@ -0,0 +1,48 @@
+var bullet
+var wall,thickness;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  
+  bullet=createSprite(50,200,50,20);
+  bullet.shapeColor=('white');
+
+  wall=createSprite(1200, 200, thickness, height/2);
+  wall.shapeColor=color(80,80,80);
+  
+  thickness = random(22,83);
+  weight = random(30,52);
+  speed = random(223,321);
+}
+
+function draw() {
+  background("black");  
+  bullet.velocityX = speed;
+
+  if(hasCollided(bullet,wall)){
+bullet.velocityX=0;
+var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+ 
+   
+  if (damage>10){
+    wall.shapeColor=color(255,0,0);
+  }
+
+  if (damage<10){
+    wall.shapeColor=color(0,255,0);
+  }
+}
+
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+bulletRightEdge=lbullet.x+lbullet.width;
+wallLeftEdge=lwall.x;
+
+if (bulletRightEdge>=wallLeftEdge){
+  return true; 
+}
+return false;
+}
